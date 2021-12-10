1cddcc7fda3569c057a3489b0bb48bb33f63b440
ngoc minh <ttngocminh214@gmail.com>
2021-12-10 13:41:32 +0700

    key

--- connector.py
+++ connector.py
@@ -4,13 +4,13 @@ import psycopg2
 try:
     # connect to the PostgreSQL server
     conn = psycopg2.connect(host="10.96.191.228",
-    port= '5432',
-    database="appstore", 
-    user="postgres", 
-    password="postgres",
-    sslmode = 'require',
-    sslrootcert = '/ramfs/secrets/zoomdatapg/zoomdatapgusr.cert.pem',
-    sslkey='/ramfs/secrets/zoomdatapg/zoomdatapgusr.key.pk8'
+        port= '5432',
+        database="appstore", 
+        user="postgres", 
+        password="postgres",
+        sslmode = "require",
+        sslcert = "/ramfs/secrets/zoomdatapg/zoomdatapgusr.cert.pem",
+        sslkey="/ramfs/secrets/zoomdatapg/zoomdatapgusr.key.pem"
     )
 
     # create a cursor
