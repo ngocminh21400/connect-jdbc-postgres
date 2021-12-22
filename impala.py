63470fe5a952ea7e2a515963985cdd864dce1d85
ngoc minh <ttngocminh214@gmail.com>
2021-12-22 13:38:10 +0700

    impala

--- impala.py
+++ impala.py
@@ -1,8 +1,6 @@
 from impala.dbapi import connect
-
-conn = connect(host='hadoop-data.default.svc.cluster.local', port=21050)
+conn = connect('<Impala Daemon>', port=21050)
 cursor = conn.cursor()
-cursor.execute('SHOW DATABASES;')
-# print cursor.description  # prints the result set's schema
-results = cursor.fetchall()
-print(results)
+cursor.execute('SHOW DATABASES')
+cursor.fetchall()
+print(cursor)
