af0057b632e05bbdbd7d992696f151e04846a239
ngoc minh <ttngocminh214@gmail.com>
2021-12-23 10:00:38 +0700

    update

--- connect_impala.py
+++ connect_impala.py
@@ -6,5 +6,5 @@ import os
 conn = connect(host='hadoop-data.default.svc.cluster.local', port=21050, use_ssl = True, database = 'testdb', user = 'impala', kerberos_service_name = 'impala', auth_mechanism = 'GSSAPI')
 cursor = conn.cursor()
 cursor.execute('SHOW DATABASES;')
-cursor.fetchall()
-print(cursor)
+databases = cursor.fetchall()
+print(databases)
