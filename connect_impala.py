d76f927d1b0decc8e457464ae39bb8a09152a032
ngoc minh <ttngocminh214@gmail.com>
2021-12-23 09:59:06 +0700

    update

--- connect_impala.py
+++ connect_impala.py
@@ -3,7 +3,7 @@ import ssl
 import os
 # impala://hadoop-data.default.svc.cluster.local:21050/default;principal=impala/hadoop-data.default.svc.cluster.local@cluster.local
 
-conn = connect(host='hadoop-data.default.svc.cluster.local', port=21050,use_ssl = True, database = 'default', user = 'urusername', kerberos_service_name = 'impala', auth_mechanism = 'GSSAPI')
+conn = connect(host='hadoop-data.default.svc.cluster.local', port=21050, use_ssl = True, database = 'testdb', user = 'impala', kerberos_service_name = 'impala', auth_mechanism = 'GSSAPI')
 cursor = conn.cursor()
 cursor.execute('SHOW DATABASES;')
 cursor.fetchall()
